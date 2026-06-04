3c11542f91ee278f7e5a3a6873efe7f9f8142fa1
LiamKirkland <liam.b.kirkland@gmail.com>
2026-06-04 12:01:22 -0400

    Improve tilt effect

--- src/script.js
+++ src/script.js
@@ -390,42 +390,58 @@ function addToCollection(cardObj) {
 const tiltSettings = {
   max: 25,
   perspective: 1000,
-  scale: 1.05
+  scale: 1.05,
+  foilShift: 18
 }
+
+const collWrapper = getByID("collection-img-wrapper")
 
 searchImg.addEventListener('mouseenter', cardMouseEnter)
 searchImg.addEventListener('mousemove', cardMouseMove)
 searchImg.addEventListener('mouseleave', cardMouseLeave)
 
-collImg.addEventListener('mouseenter', cardMouseEnter)
-collImg.addEventListener('mousemove', cardMouseMove)
-collImg.addEventListener('mouseleave', cardMouseLeave)
+collWrapper.addEventListener('mouseenter', cardMouseEnter)
+collWrapper.addEventListener('mousemove', cardMouseMove)
+collWrapper.addEventListener('mouseleave', cardMouseLeave)
 
 function cardMouseEnter(event) {
-  setTransition(event.target)
+  setTransition(event.currentTarget)
+  if(event.currentTarget === collWrapper) setTransition(foilOverlay)
 }
 
 function cardMouseMove(event) {
-  const card = event.target
-  const cardWidth = card.offsetWidth
-  const cardHeight = card.offsetHeight
-  const centerX = card.offsetLeft + cardWidth / 2
-  const centerY = card.offsetTop + cardHeight / 2
+  const card = event.currentTarget
+  const rect = card.getBoundingClientRect()
+  const centerX = rect.left + rect.width / 2
+  const centerY = rect.top + rect.height / 2
   const mouseX = event.clientX - centerX
   const mouseY = event.clientY - centerY
-  const rotateX = tiltSettings.max*mouseY/(cardHeight/2).toFixed(2)
-  const rotateY = -tiltSettings.max*mouseX/(cardWidth/2).toFixed(2)
+  const rotateX = mouseY/(rect.height/2).toFixed(2)
+  const rotateY = mouseX/(rect.width/2).toFixed(2)
 
-  card.style.transform = `perspective(${tiltSettings.perspective}px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale3d(${tiltSettings.scale}, ${tiltSettings.scale}, ${tiltSettings.scale})`
+  card.style.transform = `perspective(${tiltSettings.perspective}px) rotateX(${tiltSettings.max*rotateX}deg) rotateY(${-tiltSettings.max*rotateY}deg) scale3d(${tiltSettings.scale}, ${tiltSettings.scale}, ${tiltSettings.scale})`
+
+  if (card === collWrapper) {
+    const foilX = 13 - rotateX * (tiltSettings.foilShift / 2)
+    const foilY = 10 - rotateY  * (tiltSettings.foilShift / 2)
+    foilOverlay.style.objectPosition = `${foilX}% ${foilY}%`
+  }
 }
 
 function cardMouseLeave(event) {
-  event.target.style.transform =`perspective(${tiltSettings.perspective}px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)`
-  setTransition(event.target)
+  event.currentTarget.style.transform =`perspective(${tiltSettings.perspective}px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)`
+  setTransition(event.currentTarget)
+
+  if(event.currentTarget === collWrapper) {
+    foilOverlay.style.transform = `translate3d(0, 0, 1px)`
+    foilOverlay.style.objectPosition = "13% 10%"
+    setTransition(foilOverlay)
+  }
 }
 
 function setTransition(card) {
-  card.style.transition = "transform 300ms cubic-bezier(0.03, 0.98, 0.52, 0.99)"
+  card.style.transition = "transform 300ms cubic-bezier(0.03, 0.98, 0.52, 0.99)" +
+    "object-position 300ms cubic-bezier(0.03, 0.98, 0.52, 0.99)"
   setTimeout(() => {
     card.style.transition = ""
   }, 300)
